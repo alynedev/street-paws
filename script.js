04d1684a551e86a6dcb0e3634465c76ea1290b1a
Alyne <karlaalyne08@gmail.com>
2026-07-15 11:44:01 -0300

    refactor: reorganize project structure

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-var factList = [
+const factList = [
   "Mais de 200 milhões de cães e 400 milhões de gatos vivem nas ruas em condições de abandono.",
   "Cerca de 35% da população mundial de gatos e cachorros está ou nas ruas ou em abrigos à procura de um lar.",
   "Estima-se que de 4 a 5 milhões de cães de rua são sacrificados (eutanasiados) a cada ano no mundo.",
 ];
-
-var fact = document.getElementById("fact");
-var factButton = document.getElementById("factButton");
-var count = 0;
-
-factButton.addEventListener("click", displayFact);
+const fact = document.getElementById("fact");
+const factButton = document.getElementById("factButton");
+let count = 0;
 
 function displayFact() {
   fact.innerHTML = factList[count];
@@ -17,3 +14,5 @@ function displayFact() {
     count = 0;
   }
 }
+
+factButton.addEventListener("click", displayFact);
